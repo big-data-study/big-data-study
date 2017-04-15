9e95944fe0995794c6ec6bcca574555dcc805b07
Janita <zhuchenjian@huizhi-tech.com>
2017-04-15 13:37:50 +0800

    hdfs

---
+++ hdfs-demo/src/main/java/com/janita/hdfs/HDFSArchitecture.java
@@ -0,0 +1,18 @@
+package com.janita.hdfs;
+
+/**
+ * Created by Janita on 2017-04-15 13:24
+ * HDFS原理
+ */
+public class HDFSArchitecture {
+
+    /**
+     * HDFS的架构
+     * 1.文件切块
+     * 2.副本存放
+     * 3.元数据
+     * NameNode:管理（元数据）
+     * DataNode：存储数据
+     * SecondaryNameNode:同步磁盘与内存中的数据,在伪分布式及非高可用的集群才有
+     */
+}
