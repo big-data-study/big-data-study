2c4a5fed10889b661ff64f3b1a350ec34a8f8d59
Janita <zhuchenjian@huizhi-tech.com>
2017-04-15 16:13:57 +0800

    mapreduce

--- mad-reduce/src/main/java/com/janita/mapreduce/IntroduceMapReduce.java
+++ mad-reduce/src/main/java/com/janita/mapreduce/IntroduceMapReduce.java
@@ -13,5 +13,15 @@ public class IntroduceMapReduce {
      *      2.1.MRAppMaster     1个
      *      2.2.MapTask         多个
      *      2.3.ReduceTask      多个
+     *
+     *  其实就是把处理数据的过程分为两个阶段
+     *  需要等map完成之后
+     *  reduce才能根据map的结果进一步处理
      */
+
+
+
+
+
+
 }
